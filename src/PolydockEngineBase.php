7bccb63ae10fac790ab9c32f4ef4212ab9023d32
Bryan Gruneberg <hello@bryangruneberg.com>
2025-03-08 11:28:09 -0600

    Adds an exception thrown if required variable definitions are not set

--- src/PolydockEngineBase.php
+++ src/PolydockEngineBase.php
@@ -2,18 +2,30 @@
 
 namespace FreedomtechHosting\PolydockApp;
 
+use FreedomtechHosting\PolydockApp\Exceptions\PolydockEngineValidationException;
+use FreedomtechHosting\PolydockApp\Traits\PolydockAppLoggerTrait;
+
 abstract class PolydockEngineBase implements PolydockEngineInterface
 {
+    use PolydockAppLoggerTrait;
+
     /**
-     * Validate that an app instance has all variables
+     * Validate that an app instance has all required variables
      * @param PolydockAppInstanceInterface $appInstance The app instance to validate
-     * @return bool True if the app instance has all variables, false otherwise
+     * @return bool True if the app instance has all required variables, false otherwise
+     * @throws PolydockEngineValidationException If a required variable is missing
      */
-    public function validateAppInstanceHasAllVariables(PolydockAppInstanceInterface $appInstance): bool
+    public function validateAppInstanceHasAllRequiredVariables(PolydockAppInstanceInterface $appInstance): bool
     {
         foreach($appInstance->getApp()->getVariableDefinitions() as $variableDefinition) {
             if(!$appInstance->getKeyValue($variableDefinition->getName())) {
-                return false;
+                throw new PolydockEngineValidationException(
+                    sprintf(
+                        'App instance %s is missing required variable %s',
+                        $appInstance->getAppType(),
+                        $variableDefinition->getName()
+                    )
+                );
             }
         }
 
